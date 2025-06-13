db7e969ca7549f3a61af2106ca7217fb150f4fcf
FelpisLipe <176348835+FelpisLipe@users.noreply.github.com>
2025-06-13 20:26:28 -0300

    feat: validação de paciente antes da inserção na tabela

--- js/calcula-imc.js
+++ js/calcula-imc.js
@@ -12,17 +12,17 @@ for (let i = 0; i < pacientes.length; i++) {
   var peso = tdPeso.textContent;
   var altura = tdAltura.textContent;
 
-  var pesoEhValido = true;
-  var alturaEhValida = true;
-  if (peso <= 0 || peso > 650) {
-    console.log("Peso inválido.");
-    pesoEhValido = false;
-  }
+  var pesoEhValido = validaPeso(peso);
+  var alturaEhValida = validaAltura(altura);
+  // if (!pesoEhValido) {
+  //   console.log("Peso inválido.");
+  //   pesoEhValido = false;
+  // }
+  // if (!alturaEhValida) {
+  //   console.log("Altura inválida.");
+  //   alturaEhValida = false;
+  // }
 
-  if (altura <= 0 || altura >= 3) {
-    console.log("Altura inválida.");
-    alturaEhValida = false;
-  }
 
   var tdImc = paciente.querySelector(".info-imc");
   if (pesoEhValido && alturaEhValida) {
@@ -39,4 +39,21 @@ function calculaImc(peso, altura) {
   var imc = 0;
   imc = peso / (altura * altura);
   return imc.toFixed(2);
+}
+
+function validaPeso(peso) {
+ if (peso > 0 && peso < 650) {
+    return true;
+  }
+  else {
+    return false;
+  }
+}
+function validaAltura(altura) {
+  if (altura > 0 && altura <= 3) {
+    return true;
+  }
+  else {
+    return false;
+  }
 }
